1e49fcd7cc30fb9351cafcd0a995bd10c1068f72
KSamtoclef <samtoclef@gmail.com>
2026-08-06 09:20:02 +0100

    Configure Supabase registration storage

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,12 +4,20 @@ window.SITE_CONFIG = {
   minimumWhatsAppAwayMs: 3000,
   shareCooldownMs: 1500,
   facebookExitUrl: '',
+
+  supabase: {
+    url: 'https://cqnovqvmxwmfngupgtov.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbm92cXZteHdtZm5ndXBndG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyODA0NzQsImV4cCI6MjA5OTg1NjQ3NH0.ZamXPTmqVsdHu1pD1EZLxPeSqWemBsj28Y1f-NOCEZs',
+    table: 'peller_wed_email_registration'
+  },
+
   giftLinks: {
     cow: '',
     data: '',
     cash: '',
     available: ''
   },
+
   shareMessage: `*PELLER & JARVIS FANS GIFT CELEBRATION*\n\nAvailable options include Cow Gift, Data Up to 100GB, Cash Gift Up to ₦50,000 and other gifts.\n\nOpen the celebration page here:`
 };
 
